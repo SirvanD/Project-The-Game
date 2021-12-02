8b65ddad0b76485c510d0413c205454040a69b91
SirvanD <sirvansh@gmail.com>
2021-12-02 17:24:47 +1100

    Planning_initial commit

---
+++ game.js
@@ -0,0 +1,30 @@
+/*
+- design 9 divs and wrap them using display grid container
+- design a form for users to enter their name and start game option
+- name the elements for accessibility
+- defining two choices of X and O
+- declaring two users 
+- accessing the elements on index.html
+- define handleClick functions for when user clicks on the divs
+- define the functions for conditions where a user wins,looses or draw
+- add event listeners for clicking on the divs to select one of the 2 choices
+- CSS the X and O designs/animations/transition
+
+
+- once basic works, work on the Bonus extensions
+- CSS a leaderboard for results and bonus parts/number of tries
+**/
+
+const topLeft = document.querySelector('#one')
+const topMiddle = document.querySelector('#two')
+const topRight = document.querySelector('#three')
+const middleLeft = document.querySelector('#four')
+const middleMiddle = document.querySelector('#five')
+const middleRight = document.querySelector('#six')
+const bottomLeft = document.querySelector('#seven')
+const bottomMiddle = document.querySelector('#eight')
+const bottomRight = document.querySelector('#nine')
+
+function handleUserClickedBox(){
+    
+}
